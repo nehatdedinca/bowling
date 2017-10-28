99d66fde7e409e882a7f682b10986f79e9a72422
Nehat Dedinca <nehatdedinca007@gmail.com>
2017-10-28 14:51:02 +0800

    Fix frame multiple support and update js after fix

--- src/components/controlBar/ControlBarPage.js
+++ src/components/controlBar/ControlBarPage.js
@@ -3,8 +3,11 @@ import PropTypes from 'prop-types';
 
 class ControlBar extends PureComponent {
     static propTypes = {
+        maxPins: PropTypes.number.isRequired,
+        finished: PropTypes.bool.isRequired,
         addUser: PropTypes.func.isRequired,
-        addScore: PropTypes.func.isRequired
+        addScore: PropTypes.func.isRequired,
+        resetGame: PropTypes.func.isRequired
     };
 
     constructor(props) {
@@ -14,6 +17,10 @@ class ControlBar extends PureComponent {
         this.onRollClick = () => this.handleRollClick();
         this.onKeyPress = (ev) => this.handleKeyPress(ev);
     }
+
+    handleResetGame() {
+        this.props.resetGame();
+     }
 
     handlePlayerClick() {
         this.props.addUser();
@@ -27,8 +34,8 @@ class ControlBar extends PureComponent {
             value = Math.floor(Math.random() * (this.props.maxPins,  1));
         } else if (value < 0) {
             value = 0;
-        } else if (value > 10) {
-            value = 10;
+        } else if (value > this.props.maxPins) {
+            value = this.props.maxPins;
         }
 
         this.props.addScore(value);
@@ -41,28 +48,40 @@ class ControlBar extends PureComponent {
         }
     }
 
+    renderFinish() {
+        return (
+            <div className="inner">
+                <span className="finished">Game over:</span>
+                <div className="button" onClick={this.onResetGame}>Reset</div>
+             </div>
+         );
+     }
+
+     renderControl() {
+        return (
+            <div className="inner">
+                <div className="button" onClick={this.onPlayerClick}>Add Player</div>
+                <div className="score-control">
+                    <input
+                        onKeyPress={this.onKeyPress}
+                        ref={(input) => { this.pinsInput = input; }}
+                        type="text"
+                        className="score-input"
+                        size="2"
+                        maxLength="2" />
+                    <div className="button" onClick={this.onRollClick}>Roll</div>
+                  </div>
+              </div>
+        );
+    }
+
     render() {
+        let control = this.props.finished ? this.renderFinish() : this.renderControl()
         return (
             <div className="controlbar">
-                <div className="inner">
-                    <div
-                        className="button"
-                        onClick={this.onPlayerClick}>Add Player</div>
-                    <div className="score-control">
-                        <input
-                            onKeyPress={this.onKeyPress}
-                            ref={(input) => { this.pinsInput = input; }}
-                            type="text"
-                            className="score-input"
-                            size="2"
-                            maxLength="2" />
-                        <div
-                            className="button"
-                            onClick={this.onRollClick}>Roll</div>
-                    </div>
-                </div>
+                {control}
             </div>
-        )
+        );
     }
 }
 
